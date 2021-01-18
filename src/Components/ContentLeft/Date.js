e918531e9702ac257f4cfe1e2e8325958afc41c9
RomanZakharchuk <70903505+RomanZakharchuk@users.noreply.github.com>
2021-01-18 15:15:52 +0200

    Add location

--- src/Components/ContentLeft/Date.js
+++ src/Components/ContentLeft/Date.js
@@ -2,12 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import data from "../../Data";
 import time from "../../Time";
+import useGeoLocation from "../../useGeoLocation";
 
 
 
 const Date = () => {
+    const { location, error } = useGeoLocation();
     return (
         <DateBlock>
+            <p>{location ? (
+                <p>
+                    Latitude: {location.latitude}, Longitude: {location.longitude}
+                </p>
+            ) : "Location date not available yet."}</p>
             <p>{time} PM, {data}</p>
         </DateBlock>
     )
@@ -17,4 +24,6 @@ export default Date;
 
 const DateBlock = styled.div`
 padding: 20px 0;
+overflow: hidden;
+word-wrap: break-word;
 `;
---
+++ src/useGeoLocation.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+const useGeoLocation = (options = {}) => {
+    const [error, setError] = useState();
+    const [location, setLocation] = useState();
+
+    const onSuccess = location => {
+        const { latitude, longitude } = location.coords;
+
+        setLocation({
+            latitude,
+            longitude
+        });
+    };
+
+    const onError = error => {
+        setLocation(error.message);
+    }
+
+    useEffect(() => {
+        if (!navigator.geolocation) {
+            setError("Geolocation not supported");
+            return;
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    }, [options]);
+
+    return { location, error };
+}
+
+export default useGeoLocation;
